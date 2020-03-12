95a6b475a9ce1682a99fb507b432e8b9efca572f
Rishav Sharma <rishavsharma@macbookAir.local>
2020-03-12 16:01:14 +0530

    Updates

---
+++ precache-manifest.7d7993e714a26da155a0f3af56c9d448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de6d3077ee840ce7a3e53fc37be9787",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "240be7b76db6b473fa51",
+    "url": "/monsters/static/css/main.c9251cd2.chunk.css"
+  },
+  {
+    "revision": "4c57e081a6ef00a913da",
+    "url": "/monsters/static/js/2.c35a4101.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.c35a4101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240be7b76db6b473fa51",
+    "url": "/monsters/static/js/main.774f7779.chunk.js"
+  },
+  {
+    "revision": "ff2382b9f7a936e2b476",
+    "url": "/monsters/static/js/runtime-main.f96d4c9d.js"
+  }
+]);
